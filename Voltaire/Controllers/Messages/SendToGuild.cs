0ed4e19c201be8539f1b76609488ad7e4f7acc9b
Morgan Diepart <morgandiepart@gmail.com>
2021-01-08 17:50:05 +0100

    Disable 50 messages limit

--- Voltaire/Controllers/Messages/SendToGuild.cs
+++ Voltaire/Controllers/Messages/SendToGuild.cs
@@ -56,11 +56,11 @@ namespace Voltaire.Controllers.Messages
                 return;
             }
 
-            if(!IncrementAndCheckMessageLimit.Perform(dbGuild, db))
-            {
-                await Send.SendErrorWithDeleteReaction(context, "This server has reached its limit of 50 messages for the month. To lift this limit, ask an admin or moderator to upgrade your server to Voltaire Pro. (This can be done via the `!volt admin` command.)");
-                return;
-            }
+            //if(!IncrementAndCheckMessageLimit.Perform(dbGuild, db))
+            //{
+            //    await Send.SendErrorWithDeleteReaction(context, "This server has reached its limit of 50 messages for the month. To lift this limit, ask an admin or moderator to upgrade your server to Voltaire Pro. (This can be done via the `!volt admin` command.)");
+            //    return;
+            //}
 
             var prefix = PrefixHelper.ComputePrefix(context, dbGuild);
             var channel = candidateChannels.OrderBy(x => x.Name.Length).First();
